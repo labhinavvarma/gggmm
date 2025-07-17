cba40b9008f26e610352c74847e6602b3c81ea06
L Abhinav Varma <67184999+labhinavvarma@users.noreply.github.com>
2025-07-16 21:47:34 -0400

    Update ddd.py

--- ddd.py
+++ ddd.py
@@ -1,13 +1,1 @@
 
-# Neo4j Configuration
-NEO4J_URI=neo4j://10.189.116.237:7687
-NEO4J_USER=neo4j
-NEO4J_PASSWORD=Vkg5d$F!pLq2@9vRwE=
-NEO4J_DATABASE=connectiq
-
-# Cortex LLM Configuration
-CORTEX_URL=https://sfassist.edagenaidev.awsdns.internal.das/api/cortex/complete
-CORTEX_API_KEY=78a799ea-a0f6-11ef-a0ce-15a449f7a8b0
-CORTEX_APP_ID=edadip
-CORTEX_APLCTN_CD=edagnai
-CORTEX_MODEL=llama3.1-70b
