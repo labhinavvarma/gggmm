4a48c8df10a624d9b60217dff239ccaba416a46e
L Abhinav Varma <67184999+labhinavvarma@users.noreply.github.com>
2025-09-22 14:35:21 -0400

    Update d.py

--- d.py
+++ d.py
@@ -1,1 +1,29 @@
+# Use official Node.js LTS image
+FROM quay-nonprod.elevancehealth.com/multiarchitecture-golden-base-images/ubi8-nodejs-minimal:nodejs-20 AS base
 
+# Install dependencies only when needed
+FROM base AS deps
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci --legacy-peer-deps
+
+# Rebuild the source code only when needed
+FROM base AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+RUN touch .env.local
+RUN npm run build
+
+# Production image
+FROM base AS runner
+WORKDIR /app
+ENV NODE_ENV production
+COPY --from=builder /app/public ./public
+RUN mkdir .next
+COPY --from=builder /app/.next/standalone ./
+COPY --from=builder /app/.next/static ./.next/static
+EXPOSE 3000
+ENV PORT 3000
+ENV HOSTNAME "0.0.0.0"
+CMD ["node", "server.js"]
