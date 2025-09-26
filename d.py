cfb1b54e47481cda1837169ff3ab6427705649a2
L Abhinav Varma <67184999+labhinavvarma@users.noreply.github.com>
2025-09-26 01:36:49 -0400

    Update d.py

--- d.py
+++ d.py
@@ -1,15 +1,1 @@
-FROM node:20-alpine
-WORKDIR /app
-COPY package*.json ./
-RUN npm install --legacy-peer-deps
-COPY . .
-ENV NEXT_TELEMETRY_DISABLED=1
-RUN npm run build
-EXPOSE 3000
-CMD ["npm", "start"]
 
-
-
-docker run -t my-healthagent-project .
-
-docker run -p 3000:3000 nextjs-app
